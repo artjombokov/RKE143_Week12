1c11409bb6bb80228ba9b7317aa4e959aee15c71
artjombokov <abokov@tktk.ee~>
2023-12-12 13:56:20 +0200

    week12

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+//const db = require('./db');
+const recipeRouter = require('./routes/recipes.routs');
+const ingredientRouter = require('./routes/ingredients.routes');
+const fullRecipesRouter = require('./routes/fullRecipes.routes');
+const randomRouter = require('./routes/randomRecipe.routes');
+const app = express();
+
+app.use(express.json());
+
+app.use('/ingredients', ingredientRouter);
+app.use('/recipes', recipeRouter);
+app.use('/fullRecipes', fullRecipesRouter);
+app.use('/random', randomRouter);
+
+
+app.listen(3000, () => {
+    console.log('Server is running on Port 3000.');
+});
